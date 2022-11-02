912e938f20ecd32fd77512e6ed14ae547011b398
Hashimoto Takafumi <20389292+HASHIMOTO-Takafumi@users.noreply.github.com>
2022-11-02 18:56:22 +0900

    update cli interface

--- cmd/tfparallelizer/main.go
+++ cmd/tfparallelizer/main.go
@@ -3,24 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/HASHIMOTO-Takafumi/tfparallelizer/internal"
 )
 
 func main() {
+	var c = flag.String("c", "", "YAML configuration file")
+
+	flag.Usage = usage
 	flag.Parse()
 
-	var c = flag.Arg(0)
-	var s = flag.Arg(1)
-	var a = flag.Arg(2)
-	var b = flag.Arg(3)
+	var s = flag.Arg(0)
+	var a = flag.Arg(1)
+	var b = flag.Arg(2)
 
-	if b == "" {
-		fmt.Printf("4 arguments required")
-		return
-	}
-
-	comparer := internal.New(c, s)
+	comparer := internal.New(*c, s)
 
 	comparer.Compare(a, b)
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s [flags] schema.json left_tfstate.json right_tfstate.json\n", os.Args[0])
+	flag.PrintDefaults()
 }
